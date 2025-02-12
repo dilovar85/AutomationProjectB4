cc476d1f5d41995a5e913caa028afc92337f9941
dilovar85 <dilovar85@inbox.ru>
2025-02-11 19:03:49 -0500

    actions

---
+++ src/test/java/io/loop/test/day9/T1_actions_hoverover.java
@@ -0,0 +1,29 @@
+package io.loop.test.day9;
+
+import io.loop.test.utilities.ConfigurationReader;
+import io.loop.test.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class T1_actions_hoverover {
+
+    @Test
+    public void google_action_hover(){
+        Driver.getDriver().get(ConfigurationReader.getProperties("google.url"));
+        WebElement googleSearch = Driver.getDriver().findElement(By.xpath("//input[@id='gbqfbb']/preceding-sibling::input"));
+        WebElement feelingLucky = Driver.getDriver().findElement(By.xpath("//input[@id='gbqfbb']"));
+
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(googleSearch).perform();
+
+        actions.moveToElement(feelingLucky).perform();
+
+
+
+    }
+}
